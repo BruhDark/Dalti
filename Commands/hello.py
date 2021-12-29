99f075db316afdb9f45366752f1210e43176f074
Dark <fedefunes90@gmail.com>
2021-12-29 13:13:10 -0300

    update hello.py

--- Commands/hello.py
+++ Commands/hello.py
@@ -8,7 +8,7 @@ class Hello(commands.Cog):
 
     @user_command(name="Say hello", guild_ids=[688912903385907223])
     async def hello(self, ctx, member: discord.Member):
-        await ctx.respond("{ctx.author.mention} says hello to {member.mention}")
+        await ctx.respond(f"{ctx.author.mention} says hello to {member.mention}")
 
 def setup(bot):
     bot.add_cog(Hello(bot))
