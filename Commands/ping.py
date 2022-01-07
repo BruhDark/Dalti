119db33e030d227accd018fa25503372999b743b
Dark <fedefunes90@gmail.com>
2022-01-07 15:52:28 -0300

    Make ping prettier

--- Commands/ping.py
+++ Commands/ping.py
@@ -1,5 +1,7 @@
 from discord.commands import slash_command
 from discord.ext import commands
+from config import COLORS, EMOTES
+import discord
 
 class Ping(commands.Cog):
     def __init__(self, bot):
@@ -11,7 +13,10 @@ class Ping(commands.Cog):
 
         latency = round(self.bot.latency * 1000)
 
-        await ctx.respond(f":ping_pong: Pong! My latency: `{latency}ms`")
+        oknos = EMOTES["oknos"]
+
+        Embed = discord.Embed(description=f"{oknos} My current lantecy: `{latency}ms`", color=COLORS["normal"])
+        await ctx.respond(embed=Embed)
 
 def setup(bot):
     bot.add_cog(Ping(bot))
