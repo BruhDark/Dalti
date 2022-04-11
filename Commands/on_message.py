2f128574966ae63726c102496fbf98d3f0dace23
Dark <58370174+BruhDark@users.noreply.github.com>
2022-04-11 13:20:01 -0300

    Create on_message.py

---
+++ Commands/on_message.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+
+class Message(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+
+        if message.author == self.bot.user:
+            return
+
+        if message.channel.id == 963098061024940116:
+            
+            await message.add_reaction("✅")
+            await message.add_reaction("❌")
+
+            thread = await message.create_thread("Suggestion Discussion")
+            msg = await thread.send(f":smile: Use this thread to discuss about the above suggestion.\nFeel free to propose changes, imrpovenets or ways to apply it to the bot on an afficent way.\n\n:wave: Have a good day.")
+            await msg.pin()
+
+
+def setup(bot):
+    bot.add_cog(Message(bot))
